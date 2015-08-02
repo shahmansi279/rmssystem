8cb80b0ec73afbd74c782217c016378800170f1c
nthiagu <nthiagu@gmail.com>
2015-08-02 10:49:36 -0700

    Committing member CRUD, client program, updated Member class

--- src/com/project/rms/accounts/AccountClient.java
+++ src/com/project/rms/accounts/AccountClient.java
@@ -1,17 +1,216 @@
 package com.project.rms.accounts;
+import java.util.ArrayList;
+import java.util.List;
+/*
+ * Author:  Thiagarajan Natarajan
+ * Purpose: Client code to show the menu option and get customer information
+ * Date: 08/01/2015
+ */
 
+
+import java.util.Scanner;
 public class AccountClient {
+public static  List<Member> members1 = new ArrayList<Member>();
+public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			Member member;
+			//List<Member> members = new ArrayList<Member>();
+
+			//CarList carlist = new CarList();
+
+			//Scanner s = new Scanner(System.in);
+			Scanner console1;
+			long i=0; 
+			String strmFName = null; 
+			String strmLName = null; 
+			String strmEmail = null; 
+			String strmPhone = null;
+			String strmAddress1 = null; 
+			String strmAddress2 = null; 
+			String strmCity = null; 
+			String strmState = null; 
+			String strmZipcode = null;
+			String strmPrefnotfn = null;
+			String strmPrefpmt = null; 
+			String strmCreditcard = null; 
+			String strmPrimeflag = null;
+			int choice;
+			AccountManager mops = new AccountManager();
+		  // public static List<Member> members1 = new ArrayList<Member>();
+			Member smem1;
+			member = new Member(1, "Thiagu", "Natar", null, null, null, null, null, null, null, null, null, null, null);
+			//  mops.addMembers(member);
+			//Initialize member array 
+
+			smem1 = new Member(1,"Cust1Fname","Cust1Lname","Cust01@yahoo.com","4085550001","Cust1 Address1","Cust1 Address2","Cust1 City","Cust1 State","90001","Email","Cash","1234567890120001","Y");
+		//members1.add(smem1,members1);
+			members1.add(smem1);
+			//mops.addMembers(smem1);
+			
+			smem1 = new Member(2,"Cust2Fname","Cust2Lname","Cust02@yahoo.com","4085550002","Cust2 Address1","Cust2 Address2","Cust2 City","Cust2 State","90002","Email","CreditCard","1234567890120002","Y");
+		   members1.add(smem1);
+		//	mops.addMembers(smem1);
+		   
+		   smem1 = new Member(3,"Cust3Fname","Cust3Lname","Cust03@yahoo.com","4085550003","Cust3 Address1","Cust3 Address2","Cust3 City","Cust3 State","90003","SMS","Cash","1234567890120003","Y");
+		    members1.add(smem1);
+		    smem1 = new Member(4,"Cust4Fname","Cust4Lname","Cust04@yahoo.com","4085550004","Cust4 Address1","Cust4 Address2","Cust4 City","Cust4 State","90004","Email","Cash","1234567890120004","Y");
+		    members1.add(smem1);
+		    smem1 = new Member(5,"Cust5Fname","Cust5Lname","Cust05@yahoo.com","4085550005","Cust5 Address1","Cust5 Address2","Cust5 City","Cust5 State","90005","Email","Cash","1234567890120005","Y");
+		    members1.add(smem1);
+		    smem1 = new Member(6,"Cust6Fname","Cust6Lname","Cust06@yahoo.com","4085550006","Cust6 Address1","Cust6 Address2","Cust6 City","Cust6 State","90006","Email","Cash","1234567890120006","N");
+		    members1.add(smem1);
+		    smem1 = new Member(7,"Cust7Fname","Cust7Lname","Cust07@yahoo.com","4085550007","Cust7 Address1","Cust7 Address2","Cust7 City","Cust7 State","90007","Email","Cash","1234567890120007","Y");
+		    members1.add(smem1);
+		    smem1 = new Member(8,"Cust8Fname","Cust8Lname","Cust08@yahoo.com","4085550008","Cust8 Address1","Cust8 Address2","Cust8 City","Cust8 State","90008","Email","Cash","1234567890120008","Y");
+		    members1.add(smem1);
+		    smem1 = new Member(9,"Cust9Fname","Cust9Lname","Cust09@yahoo.com","4085550009","Cust9 Address1","Cust9 Address2","Cust9 City","Cust9 State","90009","Email","Cash","1234567890120009","Y");
+		    members1.add(smem1);
+		    smem1 = new Member(10,"Cust10Fname","Cust10Lname","Cust10@yahoo.com","4085550010","Cust10 Address1","Cust10 Address2","Cust10 City","Cust10 State","90010","Email","Cash","1234567890120010","N");
+		    members1.add(smem1);
+
+		   
+			do {
+
+				System.out.println("RIDE MANAGEMENT SYSTEM - Membership Management - SystemAdmin ");
+				System.out.println("=============================================================");
+				System.out.println("1. Add Member");
+				System.out.println("2. Retrieve Member");
+				System.out.println("3. Update Member");
+				System.out.println("4. Delete Members");
+				System.out.println("5. Search Members");
+				System.out.println("6. Select Members");
+				System.out.println("7. Sort Members");
+				System.out.println("8. Back");
+				System.out.println("9. Exit");
+				System.out.println("Enter your option(1,2,3,4,5,6,7,8,9)");
+
+
+				console1 = new Scanner(System.in);
+				choice = console1.nextInt();
+				console1.nextLine();
+
+				if (choice == 1)
+
+				{
+					System.out.println("Adding members");
+					System.out.println("Adding Member Information");
+					System.out.println("Enter Member First Name");
+					console1 = new Scanner(System.in);
+					strmFName = (console1.nextLine());
+					System.out.println("Enter Member Last Name");
+					console1 = new Scanner(System.in);
+					strmLName = (console1.nextLine());
+					System.out.println("Enter Member Email");
+					console1 = new Scanner(System.in);
+					strmEmail = (console1.nextLine());
+					System.out.println("Enter Member Phone Number ");
+					console1 = new Scanner(System.in);
+					strmPhone = (console1.nextLine());
+					System.out.println("Enter Member Address 1 ");
+					console1 = new Scanner(System.in);
+					strmAddress1 = (console1.nextLine());
+					System.out.println("Enter Member Address 2 ");
+					console1 = new Scanner(System.in);
+					strmAddress2 = (console1.nextLine());
+					System.out.println("Enter City ");
+					console1 = new Scanner(System.in);
+					strmCity = (console1.nextLine());
+					System.out.println("Enter State ");
+					console1 = new Scanner(System.in);
+					strmState = (console1.nextLine());
+					System.out.println("Enter Zip ");
+					console1 = new Scanner(System.in);
+					strmZipcode = (console1.nextLine());
+					System.out.println("Enter Preferred Notification Method ");
+					console1 = new Scanner(System.in);
+					strmPrefnotfn = (console1.nextLine());
+					System.out.println("Enter Preferred Payment Method ");
+					console1 = new Scanner(System.in);
+					strmPrefpmt = (console1.nextLine());
+					System.out.println("Enter Customer Credit Card Number ");
+					console1 = new Scanner(System.in);
+					strmCreditcard = (console1.nextLine());
+					System.out.println("Enter Prime Member Flag ");
+					console1 = new Scanner(System.in);
+					strmPrimeflag = (console1.nextLine());
+					
+					/* passing the values from the scanner */
+					
+					member.setMemFname(strmFName);
+					member.setMemLname(strmLName);
+					member.setMemEmail(strmEmail);
+					member.setMemPhone(strmPhone);
+					member.setMemAddress1(strmAddress1);
+					member.setMemAddress2(strmAddress2);
+					member.setMemCity(strmCity);
+					member.setMemState(strmState);
+					member.setMemZipcode(strmZipcode);
+					member.setMemPrefnotfn(strmPrefnotfn);
+					member.setMemPrefpmt(strmPrefpmt);
+					member.setMemCreditcard(strmCreditcard);
+					member.setMemPrimeflag(strmPrimeflag);
+					
+					//mops.addMembers(member);
+					mops.addMembers(member, members1);
+					//mops.addMembers(member);
+
+				}
+
+				if (choice == 2)	
+				{
+					System.out.println("Retrieving Member ");
+					//mops.addMembers(member);
+					//	member.addMembers(member);				
+				}
+
+				if (choice == 3)	
+				{
+					System.out.println("Updating Member ");
+					//mops.addMembers(member);
+					//	member.addMembers(member);				
+				}
+				if (choice == 4)	
+				{
+					System.out.println("Deleting Member ");
+					//mops.addMembers(member);
+					//	member.addMembers(member);				
+				}			
+				if (choice == 5)	
+				{
+					System.out.println("Searching Member Member ");
+					//mops.addMembers(member);
+					//	member.addMembers(member);				
+				}	
+				if (choice == 6)	
+				{
+					System.out.println("Sorting Member ");
+				//	mops.addMembers(member);
+					//	member.addMembers(member);				
+				}	
+
+
+
+			} while (choice != 9);
+			
+			/*
+			 * long i=0; String strmFName = null; String strmLName = null; String strmEmail = null; String strmPhone = null;
+				String strmAddress1 = null; String strmAddress2 = null; String strmCity = null; String strmState = null; String strmZipcode = null;String strmPrefnotfn = null;
+		String strmPrefpmt = null; String strmCreditcard = null; String strmPrimeflag = null;
+			 * 
+			 */
+
+
+		}
 
 	
-	public static void main(String args[]){
 		
 		
 		//take input from customer
 		
 		
-		AccountManager am= new AccountManager();
-		am.addCustomer("","");
+		//AccountManager am= new AccountManager();
+		//am.addCustomer("");
 		
-			}
-	
+		
+		
 }
--- src/com/project/rms/accounts/AccountManager.java
+++ src/com/project/rms/accounts/AccountManager.java
@@ -2,30 +2,60 @@ package com.project.rms.accounts;
 
 
 /**
- * <!-- begin-user-doc -->
- * <!--  end-user-doc  -->
- * @generated
+ * 
+ * Author:  Thiagarajan Natarajan
+ * Purpose: Member CRUD operations
+ * Date: 08/01/2015
  */
 
-public class AccountManager
-{
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 */
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+	
+
+	public class AccountManager{
+		//public static List<Member> members = new ArrayList<Member>();
+		 Member sm1;
+		 
+		
+		
+		//add member
+public  void addMembers(Member m,  List<Member> members1)
+//This method add members to Member array members1
+		{
+		
+	        
+	        if (!members1.isEmpty())
+	        {
+	        	
+	        	
+	    	        sm1 = new Member(m.nMemberID,m.memFname,m.memLname,m.memEmail,m.memPhone,m.memAddress1,m.memAddress2,m.memCity,m.getMemState(),m.memZipcode,m.memPrefnotfn,m.memPrefpmt,m.memCreditcard,m.memPrimeflag);
+	    	        members1.add(sm1);
+	    	    
+	    	    System.out.println("After adding the size of the members arraylist = "
+	    				+ members1.size());
+	    		//sortMembers(members);
+	    		for (int i = 0; i < members1.size(); i++)
+	    			
+	    		{
+	    	
+	    				System.out.println("\n"+members1.get(i).getMemFname()+" * "+ members1.get(i).getMemLname()+ " * "+members1.get(i).getMemEmail() + " * "+members1.get(i).getMemPhone());
+	    				//System.out.println(members1.get(i).getMemLname());
+	        	
+	    		}
+	  }//if
+	        
+		} //method end
+
 	public AccountManager(){
 		super();
 	}
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
 	
-	public void addCustomer(String name, String phone...) {
+	
+	public void addCustomer(String name) {
 		
 		Account a = new Account();
 				a.setAcctUsername(name);
